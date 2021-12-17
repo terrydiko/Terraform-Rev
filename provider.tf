b71de9ba997bc11af1083a9a7c64c8dac3457518
Arvin <arvin_jjtech@shaw.ca>
2021-12-17 06:19:49 -0700

    backend s3

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
     region = "us-east-1"
     
 }
+
+terraform {
+  backend "s3" {
+      bucket = "arvinjjtechjjtech1"
+      key = "state/terraform.tfstate"
+      region = "us-east-1"
+    
+  }
+}
